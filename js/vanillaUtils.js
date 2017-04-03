ba1e9b9418a3ec2525a3d8cb6534797501321da6
leticiawanderley <leticiawanderley@lsd.ufcg.edu.br>
2017-04-03 09:33:25 -0300

    Refactoring :icecream: js

--- js/vanillaUtils.js
+++ js/vanillaUtils.js
@@ -1,38 +1,16 @@
-function selecionaExatas() {
-	document.getElementById("exatas").className += "btn-selected";
-	document.getElementById("saude").className = document.getElementById("saude").className.split("btn-selected").join("");
-	document.getElementById("humanas").className = document.getElementById("humanas").className.split("btn-selected").join("");
-
-	var e = document.getElementById("exatasCharts");
-	var s = document.getElementById("saudeCharts");
-	var h = document.getElementById("humanasCharts");
-	e.style.display = "block";
-	s.style.display = "none";
-	h.style.display = "none";
-}
-
-function selecionaSaude() {
-	document.getElementById("saude").className += "btn-selected";
-	document.getElementById("exatas").className = document.getElementById("exatas").className.split("btn-selected").join("");
-	document.getElementById("humanas").className = document.getElementById("humanas").className.split("btn-selected").join("");
-
-	var e = document.getElementById("exatasCharts");
-	var s = document.getElementById("saudeCharts");
-	var h = document.getElementById("humanasCharts");
-	e.style.display = "none";
-	s.style.display = "block";
-	h.style.display = "none";
-}
-
-function selecionaHumanas() {
-	document.getElementById("humanas").className += "btn-selected";
-	document.getElementById("exatas").className = document.getElementById("exatas").className.split("btn-selected").join("");
-	document.getElementById("saude").className = document.getElementById("saude").className.split("btn-selected").join("");
-
-	var e = document.getElementById("exatasCharts");
-	var s = document.getElementById("saudeCharts");
-	var h = document.getElementById("humanasCharts");
-	e.style.display = "none";
-	s.style.display = "none";
-	h.style.display = "block";
+function selecionaArea(area) {
+	let areas = {"exatas": "exatasCharts", "saude": "saudeCharts", "humanas": "humanasCharts"};
+	let chart;
+	let hiddenChart;
+	for (let a in areas) {
+		if (area === a) {
+			document.getElementById(a).className += "btn-selected";
+			chart = document.getElementById(areas[a]);
+			chart.style.display = "block";
+		} else {
+			document.getElementById(a).className = document.getElementById(a).className.split("btn-selected").join("");
+			hiddenChart = document.getElementById(areas[a]);
+			hiddenChart.style.display = "none";
+		}
+	}
 }
